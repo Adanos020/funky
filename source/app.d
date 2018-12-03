7534f660a8f421f22f361e58aa764f42934a6482
Adanos020 <adanos020@gmail.com>
2018-12-03 02:24:11 +0000

    Fixed errors in importing modules from console arguments.

--- source/app.d
+++ source/app.d
@@ -15,8 +15,33 @@ string getCommand(ref Terminal term)
         }
         catch (UserInterruptionException ex)
         {
-                // return "exit()";
-                return null;
+                return "exit()";
+        }
+}
+
+void logErrorIfOccured(ref Terminal term, ParseStatus status)
+{
+        if (status.code == StatusCode.FAILURE)
+        {
+                if (status.extra.length)
+                {
+                        status.extra = "\n" ~ status.extra;
+                }
+
+                term.color(Color.red, Color.DEFAULT);
+                term.writefln(
+                        "Error in %s, line %s: %s%s",
+                        status.moduleName,
+                        status.line,
+                        status.message,
+                        status.extra
+                );                        
+                term.reset;
+
+                if (status.moduleName != "console")
+                {
+                        // what
+                }
         }
 }
 
@@ -25,12 +50,16 @@ void main(string[] args)
         auto term = Terminal(ConsoleOutputType.linear);
 
         init(&term);
-        importModule("std/init");
+        term.logErrorIfOccured(importModule("std/init"));
 
         // Importing modules.
         foreach (arg; args[1 .. $])
         {
-                importModule(arg);
+                if (arg.endsWith(".f"))
+                {
+                        arg = arg[0 .. $ - 2];
+                }
+                term.logErrorIfOccured(importModule(arg));
         }
 
         while (true)
@@ -38,34 +67,13 @@ void main(string[] args)
                 term.write(">>> ");
 
                 const(string) command = term.getCommand;
-                if (!command)
-                {
-                        break;
-                }
-
                 ParseStatus status = command.interpret;
+
+                term.logErrorIfOccured(status);
 
                 if (status.code == StatusCode.EXIT)
                 {
                         break;
-                }
-
-                if (status.code == StatusCode.FAILURE)
-                {
-                        if (status.extra.length)
-                        {
-                                status.extra = "\n" ~ status.extra;
-                        }
-
-                        term.color(Color.red, Color.DEFAULT);
-                        term.writefln(
-                                "Error in %s, line %s: %s%s",
-                                status.moduleName,
-                                status.line,
-                                status.message,
-                                status.extra
-                        );                        
-                        term.reset;
                 }
         }
 }
